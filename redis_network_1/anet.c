d81ae774d54c4c9d6dbc3be891e72a5bd72e46c7
H00ly666 <hecai4237@gmail.com>
2018-12-13 23:35:28 +0800

    asdf

--- redis_network_1/anet.c
+++ redis_network_1/anet.c
@@ -106,12 +106,62 @@ int anetTcpAccpt (char *err, int s, char *ip, size_t ip_len, in *port)
             inet_ntop(AF_INET, (void *)&(s->addr), ip, ip_len);
         if (port)
             *port = ntohs (s->sin_port);
-    }
+   }
     return fd;
 }
 
 
+/*
+ * 
+ */
+static int _anetTcpServer(char *err, int port, char *bindaddr, int af, int backlog)
+{
 
+    int s, rv;
+    char _port[6];
+    struct addrinfo hints;
+    struct addrinfo *servinfo, *p;
+
+    /*将端口的值转换为char类型*/
+    snprintf (_port, 6, "%d", port);
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = af;
+    /*流式套接字 TCP*/
+    hints.ai_socktype = SOCK_STREAM:
+
+    if ((rv = getaddinfo(bindaddr, _port, &hints, &servinfo) != 0)) {
+        return ANET_ERR;
+    }
+
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if((s = socket(p->ai_family, p->si_socktype, p->protocol)) == -1)
+            continue;
+        if(anetSetReuseAddr(err, s) == ANET_ERR) 
+            goto error;
+        if(anetListen(err, s, p->ai_addr, p->addrlen, backlog) == ANET_ERR) 
+            goto error;
+        
+        goto end;
+    }
+
+    if (p == NULL)
+    {
+        anetSetError (err, "unable to bind socket");
+        goto error;
+    }
+
+error:
+    s = ANET_ERR;
+err:
+    freeaddrinfo(servinfo);
+    return s;
+
+}
+
+int anetTcpServer(char *err, int port, char *bindaddr, int backlog) {
+	return _anetTcpServer(err, port, bindaddr, AF_INET, backlog);
+}
 
 
 
