321c2175dd661ea25c63ad42d5d0987f65eecb06
H00ly666 <hecai4237@gmail.com>
2018-12-20 20:59:27 +0800

    今天理清了服务区收取客户端的数据的问题  一次数据分多次发过来

--- redis_network_1/networking.c
+++ redis_network_1/networking.c
@@ -7,6 +7,89 @@
 #include "redis.h"
 #include <math.h>
 
+void processInputBuffer(redisClient *c) {
+	// 尽可能地处理查询缓存区中的内容.如果读取出现short read, 那么可能会有内容滞留在读取缓冲区里面
+	// 这些滞留的内容也许不能完整构成一个符合协议的命令,需要等待下次读事件的就绪.
+	while (sdslen(c->querybuf)) {
+
+		if (!c->reqtype) {
+			if (c->querybuf[0] == '*') {
+				c->reqtype = REDIS_REQ_MULTIBULK; // 多条查询 
+			}
+			else {
+				c->reqtype = REDIS_REQ_INLINE; // 内联查询 
+			}
+		}
+		// 将缓冲区的内容转换成命令,以及命令参数
+		if (c->reqtype == REDIS_REQ_INLINE) {
+			if (processInlineBuffer(c) != REDIS_OK) break;
+		}
+		else if (c->reqtype == REDIS_REQ_MULTIBULK) {
+			if (processMultibulkBuffer(c) != REDIS_OK) break;
+		}
+		else {
+			// todo
+		}
+
+		if (c->argc == 0) {
+			resetClient(c); // 重置客户端
+		}
+		else {
+			if (processCommand(c) == REDIS_OK)
+				resetClient(c);
+		}
+	}
+}
+
+/*
+ * 读取客户端的查询缓冲区内容
+ */
+void readQueryFromClient(aeEventLoop *el, int fd, void *privdata, int mask) {
+	redisClient *c = (redisClient *)privdata;
+	int nread, readlen;
+	size_t qblen;
+
+	server.current_client = c; // 设置服务器的当前客户端
+
+	readlen = REDIS_IOBUF_LEN; // 读入长度,默认为16MB
+
+	// 获取查询缓冲区当前内容的长度 
+	qblen = sdslen(c->querybuf); 
+
+	/* 为查询缓冲区分配空间 */
+	c->querybuf = sdsMakeRoomFor(c->querybuf, readlen);
+	// 读入内容到查询缓存
+	nread = read(fd, c->querybuf + qblen, readlen);
+
+	if (nread == -1) {
+		if (errno == EAGAIN) {
+			nread = 0;
+		} 
+		else {
+			//freeClient(c);
+			return;
+		}
+	}
+	else if (nread == 0) { // 对方关闭了连接
+		// todo
+		//freeClient(c);
+		return;
+	}
+
+	if (nread) {
+		sdsIncrLen(c->querybuf, nread);
+	} 
+	else {
+		// 在 nread == -1 且 errno == EAGAIN 时运行
+		server.current_client = NULL;
+		return;
+	}
+	//
+	// 从查询缓存中读取内容,创建参数,并执行命令,函数会执行到缓存中的所有内容都被处理完为止
+	//
+	processInputBuffer(c);
+	server.current_client = NULL;
+}
 
 
 /*
